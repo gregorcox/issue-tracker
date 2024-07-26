28ff55e6c229ae0bb9cbdb274b7e722c3761cbf7
Gregor Cox <gregorcox@gmail.com>
2024-07-26 11:14:13 +0100

    set up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
-  return <div></div>;
+  return <Button>New issue</Button>;
 };
 
 export default IssuesPage;
