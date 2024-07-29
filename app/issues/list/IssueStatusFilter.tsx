49f914e11129fa708abdb4d9912ac4b751a4853b
Gregor Cox <gregorcox@gmail.com>
2024-07-29 19:40:05 +0100

    make columns sortable

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,17 +2,26 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
+
   const statuses: { label: string; value?: Status }[] = [
     { label: "All" },
     { label: "Open", value: "OPEN" },
     { label: "In Progress", value: "IN_PROGRESS" },
     { label: "Closed", value: "CLOSED" },
   ];
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status !== "None" ? "?status=" + status : "";
+        router.push("/issues/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
